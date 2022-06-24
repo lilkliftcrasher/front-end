76346c30b3699978ec4644f04cab759ba133f636
lilkliftcrasher <crasherkliftk@gmail.com>
2022-06-24 11:04:08 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,30 +1,23 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import axios from "axios";
 import Services from "./components/Services/Services";
 
 
 const App = () => {
 
-  const service=[
-    {
-        id: 1,
-        icon: "ICON",
-        title: "Product Design",
-        content:"Let me help you on the core task now, validate the design solution,collect your own data understand your users, and don't just copy your competit."
-    },
-    {
-        id: 2,
-        icon: "ICON",
-        title: "Front-end Engineering",
-        content:"Let me help you on the core task now, validate the design solution,collect your own data understand your users, and don't just copy your competit."
-    },
-    {
-        id: 3,
-        icon: "ICON",
-        title: "Teaching",
-        content:"Let me help you on the core task now, validate the design solution,collect your own data understand your users, and don't just copy your competit."
-    }
-]
+  const  [service, setService] = useState([])
+  
+
+  
+  useEffect(()=>{ 
+    axios.get('http://localhost:3001/services').then(response=>{
+
+    setService(response.data);
+    
+    })
+  
+  },[])
+
 
 
  
